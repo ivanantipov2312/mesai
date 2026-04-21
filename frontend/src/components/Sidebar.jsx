1386cc63181c7be5149b3fcad048916689be2e7d
ivanantipov2312 <ivanantipov231@gmail.com>
2026-04-21 16:43:56 +0300

    sidebar fix

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,31 +1,36 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
+	const linkStyle = ({ isActive }) =>
+		isActive
+			? "block bg-primary bg-primary rounded-xl px-4 py-3"
+			: "block px-4 py-3 hover:bg-white/10 rounded-x1"
+
   return (
     <aside className="w-72 bg-darknav text-white min-h-screen rounded-r-3xl p-6 shadow-xl">
       <h1 className="text-3xl font-bold mb-10">MESA.I</h1>
 
       <nav className="space-y-3">
 
-        <Link className="block bg-primary rounded-xl px-4 py-3" to="/">
+        <NavLink className={linkStyle} to="/" end>
           Home
-        </Link>
+        </NavLink>
 
-        <Link className="block px-4 py-3 hover:bg-white/10 rounded-xl" to="/calendar">
+        <NavLink className={linkStyle} to="/calendar">
           Calendar
-        </Link>
+        </NavLink>
 
-        <Link className="block px-4 py-3 hover:bg-white/10 rounded-xl" to="/assignments">
+        <NavLink className={linkStyle} to="/assignments">
           Assignments
-        </Link>
+        </NavLink>
 
-        <Link className="block px-4 py-3 hover:bg-white/10 rounded-xl" to="/events">
+        <NavLink className={linkStyle} to="/events">
           Events
-        </Link>
+        </NavLink>
 
-        <Link className="block px-4 py-3 hover:bg-white/10 rounded-xl" to="/recommendations">
+        <NavLink className={linkStyle} to="/recommendations">
           AI Advice
-        </Link>
+        </NavLink>
 
       </nav>
     </aside>
